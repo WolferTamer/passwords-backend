6ce6f647f4ad416c7ed9c1bef75a556b395fd376
Caleb C <wolfertamer@gmail.com>
2024-11-11 13:35:26 -0800

    Fixed issues with dependencies. This commit likely makes the login system unusable for the moment, but will be fixed in future commits

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,7 +14,7 @@ from rest_framework.authtoken.models import Token
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .encryption import encryption, decrypt_msg, gen_key
+from .cryption import encryption, decrypt_msg, gen_key
 
 from .permissions import IsOwnerPermission
 
